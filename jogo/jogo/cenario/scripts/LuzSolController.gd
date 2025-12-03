7b65d56c46a8c51041a0ee7f3b67efead10a1073
Filipe Pio Magalhães <filipepio2005@hotmail.com>
2025-12-02 23:42:24 -0300

    Remove trash

--- jogo/jogo/cenario/scripts/LuzSolController.gd
+++
@@ -1,7 +0,0 @@
-extends DirectionalLight3D
-
-func _ready() -> void:
-	rotation_degrees.y = -90
-
-func _process(delta: float) -> void:
-	rotation_degrees.y -= 0.001
--- jogo/jogo/cenario/scripts/SolController.gd
+++
@@ -1,7 +0,0 @@
-extends MeshInstance3D
-		
-var rotation_speed_deg: float = 10 
-
-func _process(delta: float) -> void:
-	#girar ao redor do seu eixo
-	rotation_degrees.y += rotation_speed_deg * delta
